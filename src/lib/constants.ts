daf7d8202c0a1623c9aa38e0ed562f15bf3625db
swan3401-alt <neverchill03@gmail.com>
2026-07-08 01:31:25 +0500

    tweak: revert these changes later

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,5 +4,8 @@ export const PENDING_TTL_MINUTES = 10;
 export const AWAITING_PAYMENT_TTL_MINUTES = 30;
 
 // Where customers send the transfer
-export const PAYMENT_CARD_NUMBER = "5614 6812 1912 8091";
-export const PAYMENT_CARD_HOLDER = "FAYZIDDIN QUTBIDINOV";
+// export const PAYMENT_CARD_NUMBER = "5614 6812 1912 8091";
+// export const PAYMENT_CARD_HOLDER = "FAYZIDDIN QUTBIDINOV";
+
+export const PAYMENT_CARD_NUMBER = "1234 1234 1234 1234";
+export const PAYMENT_CARD_HOLDER = "Card Holder Name";
